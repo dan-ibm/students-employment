64085243366e6800d8b4051d00c3945a18fb011f
Galiaskarov Ilyas <63122815+21lyas@users.noreply.github.com>
2020-04-10 21:08:38 +0600

    Update navbar.blade.php
    Починил навбар в общем проект

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,21 +1,24 @@
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+<div class="container mb-5 py-2">
 
-    <!-- Links -->
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark  align-items-center">
+        <a class="navbar-brand">Welcome back</a>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="text-success" >{{ Session::get('username') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
+                </li>
+            </ul>
+            <form class="form-inline mt-2 mt-md-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+</div>
 
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" href="/vacancies/all">{{ Session::get('username') }}</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('logout')}}">Logout</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link 3</a>
-        </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-
-</nav>
