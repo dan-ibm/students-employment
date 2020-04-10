14fa7f89228b75981d144af1c04064917ec2a6cd
Galiaskarov Ilyas <63122815+21lyas@users.noreply.github.com>
2020-04-10 21:35:56 +0600

    Update dashboard.blade.php

--- resources/views/employers/dashboard.blade.php
+++ resources/views/employers/dashboard.blade.php
@@ -1,43 +1,47 @@
 @extends('base')
-@section('title')
-<title>Dashboard</title>
-@endsection
+@extends('layouts.navbar')
+<title>Dashboard Employer</title>
+
+
 @section('main')
-<div class="container-fluid">
-    <div class="row no-gutter">
-        <div class="d-none d-md-flex col-md-3"></div>
-        <div class="col-md-8 col-lg-6">
-            <div class="login d-flex align-items-center py-5">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-9 col-lg-8 mx-auto">
-                            <h3 class="login-heading mb-4">My Dashboard</h3>
-                            <div class="card">
-                                <div class="card-body text-center">
-                                    Welcome <h3 class="text-success">{{ Auth()->user()->employer->org_name }} </h3>
-                                </div>
-                                <div class="card-body">
+<div class="container col">
+    <div class="row">
+        <div class="d-flex p-4 mb-2 rounded col-md-6 order-md-2" style="font-size: 10em">
+        <svg class="bi bi-house-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M8 3.293l6 6V13.5a1.5 1.5 0 01-1.5 1.5h-9A1.5 1.5 0 012 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 01.5-.5h1a.5.5 0 01.5.5z" clip-rule="evenodd"></path>
+            <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 011.414 0l6.647 6.646a.5.5 0 01-.708.708L8 2.207 1.354 8.854a.5.5 0 11-.708-.708L7.293 1.5z" clip-rule="evenodd"></path>
+        </svg>
 
-                                    @foreach($vacancies as $employer)
-                                        <ul>
-                                            <li><a href="list/{{$employer->id}}">{{$employer->title}}</a></li>
-                                            <p>{{ $employer->position }}</p>
-                                        </ul>
-                                    @endforeach
 
-                                </div>
-                                <div class="card-body text-center">
-                                    <a class="small" href="{{url('logout')}}"><h5 class="text-danger">Logout</h5></a>
-                                </div>
-                                <div class="card-body text-center">
-                                    <a href="{{ url('vacancies')}}" class="btn btn-primary">My vacancies</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div class="container">
+        <div>
+            <h2 class="text-success px-4">{{ Auth()->user()->employer->org_name }} </h2>
+        </div>
+
+        <div class = "row px-5">
+            <h4 class = "card-text">Your email: </h4>
+            <h4 class="text-success">{{ $employer->email }} </h4>
+        </div>
+
+        <div>
+            @foreach($vacancies as $employer)
+                <ul>
+                    <li><a href="list/{{$employer->id}}">{{$employer->title}}</a></li>
+                    <p>{{ $employer->position }}</p>
+                </ul>
+            @endforeach
+        </div>
+
+        <div class = "row px-5 py-2">
+            <a href="{{ url('vacancies')}}" class="btn btn-primary">My vacancies</a>
+        </div>
+        <div class = "row px-5 py-2">
+            <a class="small" href="{{url('logout')}}"><h5 class="text-danger">Logout</h5></a>
         </div>
     </div>
+    </div>
+    </div>
 </div>
+
+
 @endsection
